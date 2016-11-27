b4f223c6f25806163d11b83c799652ff3e7b1010
Hoan <nguyenanhhoanld@gmail.com>
2016-11-27 17:17:28 +0700

    Author: Hoan. Date: Nov 27, 2016 - 5:16PM. Description: Repair category page.

--- template/category.php
+++ template/category.php
@@ -16,6 +16,17 @@
                 $less->compileFile('less/category/type_16_junior_suite.less', 'css/category/type_16_junior_suite.css');
                 $less->compileFile('less/category/type_16_make_reservation.less', 'css/category/type_16_make_reservation.css');
             /*======================== endMine ========================*/
+            
+            /*======================== ST ========================*/
+                $less->compileFile('less/home/top-nav.less', 'css/home/top-nav.css');
+            /*======================== endST ========================*/
+
+            /*======================== T ========================*/
+                $less->compileFile('less/masterpage/type_45-footer.less', 'css/masterpage/type_45-footer.css');
+                $less->compileFile('less/category/list_1.less', 'css/category/list_1.css');
+                $less->compileFile('less/category/list_2.less', 'css/category/list_2.css');
+                $less->compileFile('less/category/list_3.less', 'css/category/list_3.css');
+            /*======================== endT ========================*/
         ?>
 
         <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
@@ -29,8 +40,18 @@
             <link rel="stylesheet" type="text/css" href="css/category/type_16_junior_suite.css">
             <link rel="stylesheet" type="text/css" href="css/category/type_16_make_reservation.css">
         <!-- ======================== endMine ======================== -->
+
+        <!-- ======================== ST ======================== -->
+            <link rel="stylesheet" type="text/css" href="css/home/top-nav.css">
+            <link rel="stylesheet" type="text/css" href="css/home/type-34.css">
+            <script type="text/javascript" src="js/script.js"></script>
+        <!-- ======================== endST ======================== -->
+
         <!-- ======================== T ======================== -->
             <link rel="stylesheet" type="text/css" href="css/masterpage/type_45-footer.css">
+            <link rel="stylesheet" type="text/css" href="css/category/list_1.css">
+            <link rel="stylesheet" type="text/css" href="css/category/list_2.css">
+            <link rel="stylesheet" type="text/css" href="css/category/list_3.css">
             <script type="text/javascript" src="js/jquery-2.2.4.min.js"></script>
             <script type="text/javascript" src="js/newjavascript.js"></script>
         <!-- ======================== T ======================== -->
@@ -38,6 +59,10 @@
 
     <body>
         <?php include'page/masterpage/weather.php'; ?>
+        <?php include'page/masterpage/top-nav.php'; ?>
+
+        <?php include'page/category/list_1.php'; ?>
+        <?php include'page/category/list_2.php'; ?>
         <?php include'page/category/round_cove.php'; ?>
         <?php include'page/category/junior_suite.php'; ?>
         <?php include'page/category/make_reservation.php'; ?>
