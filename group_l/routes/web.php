137ec003dd273c430652d58244e7728065d72f0a
Hoan <nguyenanhhoanld@gmail.com>
2016-12-05 19:38:33 +0700

    Author: Hoan. Date: Dec 5, 2016 - 7:37PM. Description: Upload module template.

--- group_l/routes/web.php
+++ group_l/routes/web.php
@@ -31,10 +31,31 @@ Route::get('/administrator', [
 	'as' => 'administrator',
 	'uses' => 'AuthenticationController@index'
 	]);*/
-Route::get('/admin', 'AdminController@index');
-Route::get('/edit', 'AdminController@edit');
-Route::get('/update', 'AdminController@update');
-Route::get('/delete', 'AdminController@delete');
-Route::get('/addView', 'AdminController@addView');
-Route::get('/add', 'AdminController@add');
-Route::get('/search', 'AdminController@searchMenu');
+//================================== Admin Page ==================================//
+Route::get('/admin/page', [
+	'as' => 'admin',
+	'uses' => 'AdminController@index'
+	]);
+Route::get('/admin/page/menu', [
+	'as' => 'admin.menu',
+	'uses' => 'AdminController@menuView'
+	]);
+//================================== Function ==================================//
+Route::get('/admin/edit', [
+	'as' => 'edit',
+	'uses' => 'AdminController@edit'
+	]);
+Route::get('/admin/delete', [
+	'as' => 'delete',
+	'uses' => 'AdminController@delete'
+	]);
+Route::get('/admin/add', [
+	'as' => 'add',
+	'uses' => 'AdminController@add'
+	]);
+Route::get('/admin/search', [
+	'as' => 'search',
+	'uses' => 'AdminController@searchMenu'
+	]);
+Route::get('/admin/uploadFileView', 'AdminController@uploadFileView');
+Route::post('/admin/uploadFile', 'AdminController@uploadFile');
